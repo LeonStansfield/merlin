79ddf46ea352aba636eb404dc9f0d0429a08358f
LeonStansfield <leonstansfield208@gmail.com>
2023-08-02 14:44:27 +0100

    Remove everything

--- gameData/scenes/sceneTwo.msd
+++
@@ -1,5 +0,0 @@
-object
-Tilemap
-tilemap
-gameData/tilemap2.txt
-8
--- includes/MerlinMath.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <math.h>
-#include <vector>
-
-#include "raylib.h"
-#include "includes/MerlinResources.h"
